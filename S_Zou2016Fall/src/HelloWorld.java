6dd3d5d5d4501cbd25e389a5386764de56d672ea
Silvia Zou <18zous32@millshighsch.org>
2016-08-30 08:33:10 -0700

    Created Hello World

--- S_Zou2016Fall/src/HelloWorld.java
+++ S_Zou2016Fall/src/HelloWorld.java
@@ -7,13 +7,12 @@
  *
  */
 public class HelloWorld {
-
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		System.out.println("Hello World");
 		// TODO Auto-generated method stub
-
 	}
-
 }
